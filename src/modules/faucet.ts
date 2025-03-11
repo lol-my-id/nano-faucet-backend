9f1a3e4267a1f1638d299e16c594d9afc185b621
nn <jaksow05@gmail.com>
2025-03-11 16:04:51 +0100

    fix: give 20% comission to ref instead of 100%
    Good thing I caught it

--- src/modules/faucet.ts
+++ src/modules/faucet.ts
@@ -136,7 +136,7 @@ export class Faucet {
                     if(data.ref_link && ExchangeService.available) {
                         try {
                             const refPrize = ExchangeService.convert(this.settings.currency, getCurrencyFromAddress(data.ref_link) as Currency, prize);
-                            this.awardReferral(data.ref_link, refPrize);
+                            this.awardReferral(data.ref_link, refPrize*0.2);
                         } catch {} // Ignore errors
                     }
 
